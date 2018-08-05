f1cc10f457a8fff58332762eec72098fc5839404
Cesar Gomez <zootechdrum@gmail.com>
2018-08-05 12:25:37 -0700

    dwnld lodash/create package.json

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 console.log('starting app');
 
+const os = require("os");
 const fs = require('fs');
+const notes = require('./notes.js')
+
+
+// let user = os.userInfo();
+
+// fs.appendFileSync('greetings.txt',`Hello ${user.username}!`);
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log("starting node.js");
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'new note';
+};
